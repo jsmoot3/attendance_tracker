5ef1e813f36b871f0cd87b92aeee3f44888e1cf9
jsmoot <jsmoot3@hotmail.com>
2019-12-10 14:58:14 -0800

    Got sessions to work as far as a list. moved images to asset folder working on problems created.

--- lib/Screens/Sessions_screen.dart
+++ lib/Screens/Sessions_screen.dart
@@ -61,6 +61,100 @@ class getCurrentSessionsState extends State<getCurrentSessions> {
     }
   }
 
+  // get image info
+  Image getImage(CurrentSession _dession) {
+    var assetImage = AssetImage("asset/images/cc4.png");
+    switch (_dession.department) {
+      case "HDH":
+        {
+          assetImage = AssetImage("asset/images/butFLife.png");
+        }
+        break;
+      case "FM":
+        {
+          assetImage = AssetImage("asset/images/AEvent.jpg");
+        }
+        break;
+      case "PD":
+        {
+          assetImage = AssetImage("asset/images/butPol.png");
+        }
+        break;
+      case "HDH-FitLife":
+        {
+          assetImage = AssetImage("asset/images/butFLife.png");
+        }
+        break;
+      case "RSCD":
+        {
+          assetImage = AssetImage("asset/images/people3.png");
+        }
+        break;
+      case "TEST":
+        {
+          assetImage = AssetImage("asset/images/people3.png");
+        }
+        break;
+      case "FL":
+        {
+          assetImage = AssetImage("asset/images/FLife75.png");
+        }
+        break;
+      case "test2":
+        {
+          assetImage = AssetImage("asset/images/people3.png");
+        }
+        break;
+      case "Theater Department":
+        {
+          assetImage = AssetImage("asset/images/people3.png");
+        }
+        break;
+      case "Recreation":
+        {
+          assetImage = AssetImage("asset/images/people3.png");
+        }
+        break;
+      case "Village":
+        {
+          assetImage = AssetImage("asset/images/people3.png");
+        }
+        break;
+      case "ARV_FM":
+        {
+          assetImage = AssetImage("asset/images/people3.png");
+        }
+        break;
+      case "ARV_HDH-FitLife":
+        {
+          assetImage = AssetImage("asset/images/butFLife.png");
+        }
+        break;
+      default:
+        {
+          assetImage = AssetImage("asset/images/people3.png");
+        }
+    }
+    //var assetImage = AssetImage("asset/images/cc4.png");
+    Image newImage = new Image(
+      image: assetImage,
+      height: 96.0,
+      width: 96.0,
+      fit: BoxFit.fitWidth,
+    );
+    return newImage;
+  }
+
+  //geting the title text
+  String getHeadtext(CurrentSession _dession) {
+    return _dession.trainingGroup;
+  }
+
+  //geting the subtitle text
+  String getSubtext(CurrentSession _dession) {
+    return _dession.day + _dession.timeOfDay;
+  }
+
   @override
   Widget build(BuildContext context) {
     String responseSess = _responseSess;
@@ -79,53 +173,87 @@ class getCurrentSessionsState extends State<getCurrentSessions> {
           padding: const EdgeInsets.all(10.0),
           itemCount: csessions == null ? 0 : csessions.length,
           itemBuilder: (BuildContext context, int index) {
-            /*
-            return new Container(
-              child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Card(
-                      child: Container(
-                        child: Text(
-                          csessions[index].department.toString(),
-                          style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
+            return Container(
+              height: 150,
+              // padding: const EdgeInsets..all(10.0),
+              child: Card(
+                elevation: 8.0,
+                margin:
+                    new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+                child: Container(
+                  //decoration:
+                  // BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+                  child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10.0),
+                      // isThreeLine: true,
+                      onLongPress: () {
+                        //TODO: do something else
+                        _noTextAlert(csessions[index]);
+                      },
+                      leading: Container(
+                        decoration: new BoxDecoration(
+                            border: new Border(
+                                right: new BorderSide(
+                                    width: 1.0, color: Colors.white24))),
+                        child: getImage(csessions[index]),
+                      ),
 
-                        //child: Text("Test-------->100 -- $index"),
-                        /*   style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.black54
-                ),*/
+                      //   Image.asset('asset/images/cc4.png', fit: BoxFit.cover),
+                      //),
+                      //   leading: CircleAvatar(
+                      //    radius: 80.0,
+                      //    backgroundImage: AssetImage('asset/images/cc4.png'),
+                      //   ),
+                      title: Text(
+                        getHeadtext(csessions[index]),
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Container(
+                        child: Row(children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              //  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text(
+                                  csessions[index].campusLocation,
+                                  style: TextStyle(
+                                    fontSize: 35,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                getSubtext(csessions[index]),
+                                style: TextStyle(
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ]),
+                      )
+
+                      /* subtitle: Text(
+                      getSubtext(csessions[index]),
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ],
-                ),
-              ),
-            );
-            */
-            return Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 10,
-                  backgroundImage: AssetImage('assets/images/cc4.jpg'),
-                ),
-                title: Text(
-                  csessions[index].department,
-                  style: TextStyle(
-                    fontSize: 62,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                subtitle: Text(
-                  csessions[index].day,
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                  ),
+                    */
+
+                      //  trailing: Icon(Icons.keyboard_arrow_right),
+                      ),
                 ),
               ),
             );
@@ -178,6 +306,27 @@ class getCurrentSessionsState extends State<getCurrentSessions> {
         return ListTile(
           //title: Text(csessions[index].Department),
           title: Text("Test-------->100 -- $index"),
+        );
+      },
+    );
+  }
+
+  _noTextAlert(CurrentSession _csess) {
+    String temp = _csess.trainingGroup + _csess.campusLocation;
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Not in stock'),
+          content: new Text(temp),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
         );
       },
     );
